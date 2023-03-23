fef11a7a00098dc0f185520bc8e4c2520843d227
Yurii <shadowjura82@gmail.com>
2023-03-23 21:28:20 +0100

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        byte byteVariable = 120;
+        short shortVariable = 300;
+        int intVariable = 3000;
+        long longVariable = 30000L;
+        float floatVariable = 1.0f;
+        double doubleVariable = 23.33334;
+        System.out.println("Значение переменной byteVariable с типом byte равно " + byteVariable);
+        System.out.println("Значение переменной shortVariable с типом short равно " + shortVariable);
+        System.out.println("Значение переменной intVariable с типом int равно " + intVariable);
+        System.out.println("Значение переменной longVariable с типом long равно " + longVariable);
+        System.out.println("Значение переменной floatVariable с типом float равно " + floatVariable);
+        System.out.println("Значение переменной doubleVariable с типом double равно " + doubleVariable);
     }
 }
+
