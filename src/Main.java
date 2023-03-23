9b41f61b4f035fc39ee288cf1b9c6f5de14d3367
Yurii <shadowjura82@gmail.com>
2023-03-23 21:46:41 +0100

    task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -16,6 +17,16 @@ public class Main {
         System.out.println("Значение переменной longVariable с типом long равно " + longVariable);
         System.out.println("Значение переменной floatVariable с типом float равно " + floatVariable);
         System.out.println("Значение переменной doubleVariable с типом double равно " + doubleVariable);
+    }
+
+    public static void task2() {
+        float fVar = 27.12f;
+        long lVar = 987678965549L;
+        float fVar2 = 2.786f;
+        short sVar = 569;
+        short sVar2 = -159;
+        int iVar = 27897;
+        byte bVar = 67;
     }
 }
 
